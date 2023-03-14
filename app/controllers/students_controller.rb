8fa2cf91ba31fda4f1b10a83dc6b4f143566c145
yurubahmed <yurubiftin2002@gmail.com>
2023-03-14 09:34:28 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student = Student.find(params[:id])
+    render json: student  
+  end
 
 end
